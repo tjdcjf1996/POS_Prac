46b0148c683fa5123b5edd93de08d454e0b9c53f
GDORI <r_louis@naver.com>
2025-12-01 15:51:43 +0900

    refactor: 기존 버츄얼프린터 클래스에 프린트뷰어 설정

--- package/edge-bridge/src/hardware/virtualPrinter.ts
+++ package/edge-bridge/src/hardware/virtualPrinter.ts
@@ -1,13 +1,25 @@
+import { PrintViewer } from 'package/edge-bridge/src/core/PrintViewer.js';
 import { IPrinter } from './interfaces';
 
 export class VirtualPrinter implements IPrinter {
+  constructor(private viewer?: PrintViewer) {}
+
   async connect(): Promise<void> {
     console.log('Virtual Printer connected.');
   }
 
   async print(text: string): Promise<void> {
     console.log('Printing to Virtual Printer...');
-    await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate delay
-    console.log('Virtual Printer Output : ', text);
+
+    // 시간 지연
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // 뷰어가 있으면 출력
+    if (this.viewer && this.viewer.isOpen()) {
+      this.viewer.printToScreen(text);
+    } else {
+      // 뷰어가 없으면 콘솔
+      console.log(text);
+    }
   }
 }
